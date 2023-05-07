a5b01b9ad8da4c5e38ff5d97417756228b0b0723
Xeropo <hoangdule16@gmail.com>
2023-05-07 06:35:21 +0200

    Profile page complete and beginning of Home page

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,14 +1,47 @@
 package com.example.littlelemon
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
-fun Home(){
-    Column() {
-        Text(text = "Hello World")
+fun Home(navController: NavController){
+    Column(modifier = Modifier
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+            ) {
+            Header()
+            Button(onClick = { navController.navigate(Profile.route) },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.White, contentColor = Color.White),
+                modifier = Modifier
+                    .align(Alignment.TopEnd),
+                shape = CircleShape,
+                elevation = ButtonDefaults.elevation(0.dp)
+                ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profil),
+                    contentDescription = "Header logo",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(75.dp).padding(10.dp)
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -6,7 +6,6 @@ import android.content.SharedPreferences
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,23 +14,23 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.littlelemon.ui.theme.*
 
 
+
 @Composable
 fun Onboarding(navController: NavController, sharedPreferences: SharedPreferences){
     val context = LocalContext.current
-    Column(Modifier.fillMaxSize(),) {
+    Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
         Header()
         Box(modifier = Modifier
             .background(FirstPrimaryColor)
@@ -72,7 +71,7 @@ fun Header(){
         contentDescription = "Header logo",
         modifier = Modifier
             .width(200.dp)
-            .height(100.dp),
+            .height(100.dp)
     )
 }
 
